872089059dd1a6e7e63c09e011dee7a93dcb19aa
richiekamota <richiekamota@gmail.com>
2022-07-07 21:56:17 +0200

    Added options looping

--- src/Scrape.php
+++ src/Scrape.php
@@ -16,6 +16,8 @@ class Scrape
         $pages = $document->filter('#pages .px-6')->count();
 
         // https://www.magpiehq.com/developer-challenge/smartphones/?page={pagenumber}
+
+        $myproducts = [];
         
         for ($i=1; $i <= $pages; $i++) {
            
@@ -25,84 +27,97 @@ class Scrape
 
             $products = $document->filter('.product');
 
-            foreach ($products as $product) {
+        
 
+            foreach ($products as $product) {
+        
                 $productCrawler = new Crawler($product);
 
-                $title = $productCrawler->filter(".text-blue-600")->text();
+                $options = $productCrawler->filter('.px-2')->count();
 
-                $this->products['title'] = $title;
+                for ($o=1; $o <= $options; $o++) {
 
-                echo $title. "\n";
+                    // $productCrawler = new Crawler($option);
 
-                $productCrawler->filter('.text-blue-600')->children()->each(function(Crawler $spans) { 
+                    $title = $productCrawler->filter(".text-blue-600")->text();
+    
+                    // $this->products['title'] = $title;
+    
+                    $myproducts[] = $title;
+                }
 
-                    $capacity = $spans->filter('span')->text();
 
-                    $this->products['capacityMB'] = intval($capacity)*1000;                    
 
-                });
+                // echo $title. "\n";
+
+                // $productCrawler->filter('.text-blue-600')->children()->each(function(Crawler $spans) { 
+
+                //     $capacity = $spans->filter('span')->text();
+
+                //     // $this->products['capacityMB'] = intval($capacity)*1000;                    
+
+                // });
 
               
-                $productCrawler->children(".bg-white")->each(function(Crawler $price_avails) {   
+                // $productCrawler->children(".bg-white")->each(function(Crawler $price_avails) {   
                    
-                    $img_url = $price_avails->filter('img')->attr('src');
+                //     $img_url = $price_avails->filter('img')->attr('src');
                  
-                    echo $img_url. "\n";
+                //     echo $img_url. "\n";
 
-                    $this->products['imgUrl'] = str_replace("..","https://www.magpiehq.com/developer-challenge/smartphones",$img_url); 
+                //     // $this->products['imgUrl'] = str_replace("..","https://www.magpiehq.com/developer-challenge/smartphones",$img_url); 
 
-                    $price = $price_avails->children('div')->filter(".my-8")->text();
+                //     $price = $price_avails->children('div')->filter(".my-8")->text();
 
-                    echo $price. "\n";
+                //     echo $price. "\n";
 
-                    $this->products['price']= preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $price);
+                //     //$this->products['price']= preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $price);
 
-                    $price_avails->children('div')->filter(".my-4")->text();
+                //     $price_avails->children('div')->filter(".my-4")->text();
 
-                    foreach($price_avails as $price_avail){
+                //     // foreach($price_avails as $price_avail){
 
-                        $priceCrawler = new Crawler($price_avail);
+                //     //     $priceCrawler = new Crawler($price_avail);
 
-                        $price_avail = $priceCrawler->children('div')->eq(2)->text();
+                //     //     $price_avail = $priceCrawler->children('div')->eq(2)->text();
 
-                        echo $price_avail. "\n";
+                //     //     echo $price_avail. "\n";
 
-                        $this->products['availabilityText'] = $price_avail;
+                //     //     //$this->products['availabilityText'] = $price_avail;
                         
-                        $this->products['isAvailable'] = (preg_match("/^Availability: In Stock$/", $price_avail)) ? true:false;
+                //     //     $this->products['isAvailable'] = (preg_match("/^Availability: In Stock$/", $price_avail)) ? true:false;
 
-                    }                   
+                //     // }                   
 
-                    $price_avails->filter('div')->each(function(Crawler $shipping) {
+                //     $price_avails->filter('div')->each(function(Crawler $shipping) {
                         
-                        echo $shipping->text(). "\n";
+                //         echo $shipping->text(). "\n";
 
-                        $this->products['shippingText'] = $shipping->text();
+                //         //$this->products['shippingText'] = $shipping->text();
                         
-                        $this->products['shippingDate'] = $this->standard_date_format($shipping->text());
+                //         //$this->products['shippingDate'] = $this->standard_date_format($shipping->text());
                     
-                    });
-                });
+                //     });
+                // });
 
-                $productCrawler->filter('.flex')->children()->each(function(Crawler $flex) {
+                // $productCrawler->filter('.flex')->children()->each(function(Crawler $flex) {
                 
-                    $flex->filter('.px-2')->children()->each(function(Crawler $span) {
+                //     $flex->filter('.px-2')->children()->each(function(Crawler $span) {
 
-                    $data_colour = $span->filter('span')->eq(0)->attr('data-colour');
+                //     $data_colour = $span->filter('span')->eq(0)->attr('data-colour');
 
-                    $this->products['colour'] = $data_colour;
+                //     //$this->products['colour'] = $data_colour;
 
-                    echo $data_colour. "\n";        
-                    });
-                });  
+                //     echo $data_colour. "\n";        
+                //     });
+                // });  
            
             }
         }
         // We must not forget to de-dupe
                
 
-        file_put_contents('output.json', json_encode($this->products));
+        file_put_contents('output.json', json_encode($myproducts));
     }
 
     public function standard_date_format($str) {
